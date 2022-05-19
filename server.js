588ec8b6a1fd7c5d22be596eb096607985a40af7
Guy Farley <guy.e.farley@gmail.com>
2022-05-18 21:15:52 -0700

    Connected to weather API

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ console.log('Our first server');
 const express = require('express');
 let data = require('./data/weather.json');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 // USE
@@ -20,18 +21,36 @@ app.get('/', (request, response) => {
   response.send('Hello from my server');
 });
 
-app.get('/weather', (request, response, next) => {
+// app.get('/weather', (request, response, next) => {
+//   try {
+//     let cityFromRequest = request.query.city_name;
+
+//     let selectedCity = data.find(city => city.city_name.toLowerCase() === cityFromRequest.toLowerCase());
+
+//     let dataToSend = selectedCity.data.map(city => new Forecast(city));
+//     console.log(dataToSend);
+//     response.send(dataToSend);
+
+//   } catch (error) {
+
+//     next(error);
+//   }
+// });
+
+app.get('/weather', async (request, response, next) => {
   try {
-    let cityFromRequest = request.query.city_name;
+    let searchLat = request.query.latitude;
+    let searchLon = request.query.longitude;
 
-    let selectedCity = data.find(city => city.city_name.toLowerCase() === cityFromRequest.toLowerCase());
+    let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${searchLat}&lon=${searchLon}`;
+    let forecastFromAPI = await axios.get(weatherURL);
+    console.log(forecastFromAPI);
 
-    let dataToSend = selectedCity.data.map(city => new Forecast(city));
-    console.log(dataToSend);
+    let dataToSend = forecastFromAPI.data.data.map(city => new Forecast(city));
+    // console.log(dataToSend);
     response.send(dataToSend);
 
   } catch (error) {
-
     next(error);
   }
 });
