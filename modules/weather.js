937a9c88d487a6e64216201db4358e0821bf0f63
Guy Farley <guy.e.farley@gmail.com>
2022-05-20 18:21:44 -0700

    Moved weather and movie functions into separate modules

---
+++ modules/weather.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const axios = require('axios');
+
+async function getWeather(request, response, next) {
+
+  try {
+    let searchLat = request.query.latitude;
+    let searchLon = request.query.longitude;
+
+    let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${searchLat}&lon=${searchLon}`;
+    let forecastFromAPI = await axios.get(weatherURL);
+    console.log(forecastFromAPI);
+
+    let dataToSend = forecastFromAPI.data.data.map(city => new Forecast(city));
+    // console.log(dataToSend);
+    response.send(dataToSend);
+
+  } catch (error) {
+    // next(error);
+    Promise.resolve().then(() => {
+      throw new Error(error.message);
+    }).catch(next);
+  }
+}
+
+class Forecast {
+  constructor(cityObject) {
+    this.description = cityObject.weather.description;
+    this.date = cityObject.datetime;
+  }
+}
+
+module.exports = getWeather;
