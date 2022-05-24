390ca21dc0f142985feda2166cb092a6137cbc10
Guy Farley <guy.e.farley@gmail.com>
2022-05-23 17:03:25 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function weatherHandler(request, response) {
     });
 }
 
-app.use((error, request, response) => {
+app.use((error, request, response, next) => {
   console.log(error.message);
   response.status(500).send(error.message);
 });
